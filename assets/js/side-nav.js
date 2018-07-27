90a97e36c1c93a16955f78f8031f375e2d7680ad
Mike Lui <mike.d.lui@gmail.com>
2018-07-27 14:21:20 -0400

    Sync with jekyll source

--- assets/js/side-nav.js
+++ assets/js/side-nav.js
@@ -17,19 +17,55 @@
 //
 // This further assumes that there's only margins on one side of li's
 // Otherwise, when we shrink our .ml-collapse, there won't be the expected margin collapsing
+//
+// IMPORTANT! this is tightly coupled with the accompanying side-nav.scss.
+// Make sure they are in sync.
+// TODO scope css an js values together. CSS modules, some other framework, et al.
 
 if( $(window).width() > 992 )
 {
+
+/* From Modernizr */
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
+
+
+/* Listen for a transition! */
+var transitionEvent = whichTransitionEvent();
+
 function mlshow($elem) {
   $elem.addClass('show');
   $elem.css('height', $elem.find('ul').height());
-  $elem.css('overflow', 'visible');
 }
+
+transitionEvent && $('.collapsible-sibling').on(transitionEvent, function(event) {
+	if ($(this).hasClass('show')) {
+		$(this).css('overflow', 'visible');
+	}
+	else {
+  	$(this).css('overflow', 'hidden');
+	}
+});
+
 
 function mlhide($elem) {
   $elem.removeClass('show');
-  $elem.css('height', 0);
   $elem.css('overflow', 'hidden');
+  $elem.css('height', 0);
 }
 
 function getSiblingCollapse(elem) {
@@ -110,30 +146,80 @@ function _checkCollapse () {
 }
 var checkCollapse = _checkCollapse();
 
-gumshoe.init({
-  offset: ($(window).height() * 0.4),
-  callback: function (nav) {
-    if (!nav) {
-      return;
-    }
-
-    // Deactivate any currently active parent
-    var current = document.querySelector('.active-parent');
-    if (current) {
-      current.classList.remove('active-parent');
-    }
-
-    // Check if the nav link has a parent nav
-    var parNav = nav.parent.closest('[data-gumshoe] > li');
-
-    // If the nav link has a parent item,
-    // Then add a class to that link
-    if (parNav) {
-      parNav.classList.add('active-parent');
-    }
-
-    checkCollapse(nav.parent);
+function gumshoeCallback (nav) {
+  if (!nav) {
+    return;
   }
-});
+
+  // Deactivate any currently active parent
+  var current = document.querySelector('.active-parent');
+  if (current) {
+    current.classList.remove('active-parent');
+  }
+
+  // Check if the nav link has a parent nav
+  var parNav = nav.parent.closest('[data-gumshoe] > li');
+
+  // If the nav link has a parent item,
+  // Then add a class to that link
+  if (parNav) {
+    parNav.classList.add('active-parent');
+  }
+
+  checkCollapse(nav.parent);
 }
 
+gumshoe.init({
+  offset: ($(window).height() * 0.4),
+  callback: gumshoeCallback });
+
+window.addEventListener('resize', function () {
+  // recalculate distances
+  // automatically calls gumshow.destroy()
+  gumshoe.init({
+    offset: ($(window).height() * 0.4),
+    callback: gumshoeCallback });
+});
+
+//----------------------------------------------------------------------------
+// HACKS! YAY!
+
+// Manually set widths as a hack because of: https://stackoverflow.com/a/6433475/1371191
+// Need to set width of SECOND level manually to avoid getting cut off on the x-axis from overflow: hidden
+// Need to set width of FIRST level manually to make sure it's not expanded by second level width
+var $mainUl = $('#sidenav > ul');
+$mainUl.css('width', $mainUl.find('li').outerWidth(true));
+
+var $collapseSib = $('div.collapsible-sibling');
+$collapseSib.css('width', $collapseSib.find('ul').outerWidth(true) * 1.2);
+
+// Manually resize a tooltip if it spills over into the main content
+// Can't figure out a way to do this with pure html and css that makes sense
+// Tried using overflow-wrap: break-word, but that causes the word to break on every
+// letter in combination with position: absolute, and width: auto.
+// We could set a constant width for all tooltips, but I prefer the look of dynamically
+// sized tooltips.
+//
+// Here we manually shrink, by setting a width and breaking on a word, if any of the tooltips
+// are too wide. Kind of a pain but not too much processing, and it's only done once.
+
+var $postDiv = $('body > .container > .row > div'); // main div with all post text
+var padding = parseInt($postDiv.css('padding-left'), 10);
+var leftEdge = $postDiv.offset().left + padding;
+
+var $tooltips = $('.dot-tooltip-wrapper');
+$tooltips.each(function (idx) {
+  $t = $(this)
+  $text = $t.find('.dot-tooltip')
+
+  w = $text.width()
+  rightEdge = $t.offset().left + w;
+  overlap = rightEdge - leftEdge;
+
+  if (overlap > 0) {
+    $text.css('width', w - overlap);
+    $text.css('overflow-wrap', 'break-word');
+  }
+});
+
+} // end if width > 992
